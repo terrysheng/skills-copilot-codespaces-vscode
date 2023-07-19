5ab723f8af038af3c4c3e4448a62c6ce048d0be7
tesheng <terrysheng@users.noreply.github.com>
2023-07-19 14:33:36 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,77 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
+
+// Create web server
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create comments
+const commentsByPostId = {};
+
+// Get all comments
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+// Create comment
+app.post('/posts/:id/comments', async (req, res) => {
+  // Create comment
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content, status: 'pending' });
+  commentsByPostId[req.params.id] = comments;
+
+  // Send event to event bus
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
+  });
+
+  // Send response
+  res.status(201).send(comments);
+});
+
+// Receive event from event bus
+app.post('/events', async (req, res) => {
+  console.log('Event received:', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    // Update comment
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    // Send event to event bus
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
+
+  // Send response
+  res.send({});
+});
+
+// Listen on port 4001
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
